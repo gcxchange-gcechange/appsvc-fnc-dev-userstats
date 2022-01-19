06454610997a0199ed5d6da60790d1666a18c047
stephanie4104 <stephanie4104@hotmail.com>
2022-01-19 10:41:49 -0500

    Put back at 3am

--- StoreData.cs
+++ StoreData.cs
@@ -18,7 +18,7 @@ namespace appsvc_fnc_dev_userstats
     {
         [FunctionName("StoreData")]
         //Run everyday at 3am
-        public static async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log, ExecutionContext context)
+        public static async Task Run([TimerTrigger("0 0 3 * * *")]TimerInfo myTimer, ILogger log, ExecutionContext context)
         {
             log.LogInformation($"C# Http trigger function executed at: {DateTime.Now}");
 
