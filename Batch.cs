3f90911c8d0ff6c4a42ad087a32c78532b962014
gabmor38 <gabriela.morenor@gmail.com>
2024-02-02 11:52:07 -0500

    commented out so it doesn't get used

--- Batch.cs
+++ Batch.cs
@@ -1,158 +1,158 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using Microsoft.Graph;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.Azure.WebJobs;
+//using Microsoft.Azure.WebJobs.Extensions.Http;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.Logging;
+//using Microsoft.Graph;
+//using Newtonsoft.Json;
+//using Newtonsoft.Json.Linq;
+//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using System.Net.Http;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace appsvc_fnc_dev_userstats
-{
-    public static class Batch 
-    {
-        [FunctionName("Batch")] 
+//namespace appsvc_fnc_dev_userstats
+//{
+//    public static class Batch 
+//    {
+//        [FunctionName("Batch")] 
         
-            public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.System, "get", "post", Route = null)] HttpRequest req, ILogger log, ExecutionContext context)
-            {
+//            public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.System, "get", "post", Route = null)] HttpRequest req, ILogger log, ExecutionContext context)
+//            {
 
-            IConfiguration config = new ConfigurationBuilder()
+//            IConfiguration config = new ConfigurationBuilder()
 
-           .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-           .AddEnvironmentVariables()
-           .Build();
+//           .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+//           .AddEnvironmentVariables()
+//           .Build();
 
 
-            Auth auth = new Auth();
-            var graphAPIAuth = auth.graphAuth(log);
+//            Auth auth = new Auth();
+//            var graphAPIAuth = auth.graphAuth(log);
 
-            var groupId = ""; 
+//            var groupId = ""; 
 
-            var groups = new List<string>();
+//            var groups = new List<string>();
 
-            try
-            {
-                //Create a batch request
+//            try
+//            {
+//                //Create a batch request
 
-                var batch  = new BatchRequestContent();
+//                var batch  = new BatchRequestContent();
 
-                //Request for the batch
-                var unified = "groupTypes/any(c:c eq 'Unified')";
-                var unifiedGroups = graphAPIAuth.Groups.Request().Filter(unified).Select("id, DisplayName").Top(40);
-                //var groupDrives = graphAPIAuth.Groups[groupId].Drives.Request();
+//                //Request for the batch
+//                var unified = "groupTypes/any(c:c eq 'Unified')";
+//                var unifiedGroups = graphAPIAuth.Groups.Request().Filter(unified).Select("id, DisplayName").Top(40);
+//                //var groupDrives = graphAPIAuth.Groups[groupId].Drives.Request();
 
-                //Add the request to the Batch
-                var groupsRequestId = batch.AddBatchRequestStep(unifiedGroups);
+//                //Add the request to the Batch
+//                var groupsRequestId = batch.AddBatchRequestStep(unifiedGroups);
            
-                //var drivesRequestId = batch.AddBatchRequestStep(groupDrives);
+//                //var drivesRequestId = batch.AddBatchRequestStep(groupDrives);
                 
 
-                //var allgroupsRequest = batch.AddBatchRequestStep(allgroups);
+//                //var allgroupsRequest = batch.AddBatchRequestStep(allgroups);
 
-                //Execute the batch
+//                //Execute the batch
 
-                var returnedResponse = await graphAPIAuth.Batch.Request().PostAsync(batch); 
+//                var returnedResponse = await graphAPIAuth.Batch.Request().PostAsync(batch); 
 
-                // Process batch responses
+//                // Process batch responses
 
-                var response = await returnedResponse.GetResponseByIdAsync(groupsRequestId);
-                var responseContent = response.Content.ReadAsStringAsync().Result;
-                dynamic responseBody = JsonConvert.DeserializeObject(responseContent);
-                //var responseBody = await new StreamReader(response[allgroupsID].Content.ReadAsStreamAsync().Result).ReadToEndAsync();
+//                var response = await returnedResponse.GetResponseByIdAsync(groupsRequestId);
+//                var responseContent = response.Content.ReadAsStringAsync().Result;
+//                dynamic responseBody = JsonConvert.DeserializeObject(responseContent);
+//                //var responseBody = await new StreamReader(response[allgroupsID].Content.ReadAsStreamAsync().Result).ReadToEndAsync();
 
-                // Now process the dependencies
+//                // Now process the dependencies
 
-                log.LogInformation($"Res:{responseBody.value }");
+//                log.LogInformation($"Res:{responseBody.value }");
 
-                foreach ( var item in responseBody.value ) 
-                { 
-                    //convert object to string
-                    string jsonGroupIds = JsonConvert.SerializeObject(item.id);
-                    groups.Add(jsonGroupIds);
+//                foreach ( var item in responseBody.value ) 
+//                { 
+//                    //convert object to string
+//                    string jsonGroupIds = JsonConvert.SerializeObject(item.id);
+//                    groups.Add(jsonGroupIds);
                    
-                }
-                //get the nextPageLink
-                string nextPageLink = responseBody["@odata.nextLink"];
+//                }
+//                //get the nextPageLink
+//                string nextPageLink = responseBody["@odata.nextLink"];
 
-                //add the group ids to the groups list
+//                //add the group ids to the groups list
 
-                 foreach(var id in groups)
-                    {
-                        log.LogInformation($"LIST IDS:{id}");
-                    }
-
-
-                log.LogInformation($"NEXTPGAE:{nextPageLink}");
-                log.LogInformation($"LIST:{groups.Count}");
+//                 foreach(var id in groups)
+//                    {
+//                        log.LogInformation($"LIST IDS:{id}");
+//                    }
 
 
+//                log.LogInformation($"NEXTPGAE:{nextPageLink}");
+//                log.LogInformation($"LIST:{groups.Count}");
 
-                if (nextPageLink != null)
-                {
 
-                    var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, nextPageLink);
-                    batch.AddBatchRequestStep(httpRequestMessage);
-                }
-                    var batchNxPgResponse = await graphAPIAuth.Batch.Request().PostAsync(batch);
-                    var res = await batchNxPgResponse.GetResponsesAsync();
+
+//                if (nextPageLink != null)
+//                {
+
+//                    var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, nextPageLink);
+//                    batch.AddBatchRequestStep(httpRequestMessage);
+//                }
+//                    var batchNxPgResponse = await graphAPIAuth.Batch.Request().PostAsync(batch);
+//                    var res = await batchNxPgResponse.GetResponsesAsync();
                 
-                    var groupsNP = "";
+//                    var groupsNP = "";
 
-                foreach (var resCont in res)
-                {
+//                foreach (var resCont in res)
+//                {
 
-                    if (resCont.Value.IsSuccessStatusCode)
-                    {
-                        string toJson = await new StreamReader(resCont.Value.Content.ReadAsStreamAsync().Result).ReadToEndAsync();
+//                    if (resCont.Value.IsSuccessStatusCode)
+//                    {
+//                        string toJson = await new StreamReader(resCont.Value.Content.ReadAsStreamAsync().Result).ReadToEndAsync();
 
-                        if (resCont.Value.Headers.TryGetValues("@odataNextLink", out var nextLinkValues))
-                        {
-                            nextPageLink = nextLinkValues.FirstOrDefault();
-                            log.LogInformation($"LINK:{nextPageLink}");
-                        }
+//                        if (resCont.Value.Headers.TryGetValues("@odataNextLink", out var nextLinkValues))
+//                        {
+//                            nextPageLink = nextLinkValues.FirstOrDefault();
+//                            log.LogInformation($"LINK:{nextPageLink}");
+//                        }
 
                         
-                    var result = JsonConvert.DeserializeObject(toJson);
+//                    var result = JsonConvert.DeserializeObject(toJson);
 
-                    log.Loginformation()
-
-
+//                        log.LogInformation($"{result}");
 
 
 
-                        groupsNP += result;
+
+
+//                        groupsNP += result;
 
 
 
-                    }
+//                    }
 
-                }
+//                }
 
 
 
-                log.LogInformation($"NP{groupsNP }");
+//                log.LogInformation($"NP{groupsNP }");
 
                 
 
-            }
-            catch( Exception ex)
-            {
-                log.LogInformation($"ERROR: {ex}"); 
-                return null;
-            }
+//            }
+//            catch( Exception ex)
+//            {
+//                log.LogInformation($"ERROR: {ex}"); 
+//                return null;
+//            }
 
 
-            return new OkResult();
-            }
+//            return new OkResult();
+//            }
 
 
-    }
-}
+//    }
+//}
